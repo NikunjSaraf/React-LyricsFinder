8e08dc233ea0783ff43d87f7383c1267de3a0a60
NikunjSaraf <nik.saraf1234@gmail.com>
2020-07-06 18:23:09 +0530

    Updates

---
+++ precache-manifest.b67136e5ffa3e41afc2833e02930fa1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6276d18ea72be565b8776ff90bdc2f",
+    "url": "/React-LyricsFinder/index.html"
+  },
+  {
+    "revision": "08e0284b92cc7d06ac74",
+    "url": "/React-LyricsFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "691baba615e5c45e2e91",
+    "url": "/React-LyricsFinder/static/js/2.69613132.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-LyricsFinder/static/js/2.69613132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e0284b92cc7d06ac74",
+    "url": "/React-LyricsFinder/static/js/main.28ba2294.chunk.js"
+  },
+  {
+    "revision": "e73fe0aaf6393c47f903",
+    "url": "/React-LyricsFinder/static/js/runtime-main.2c7e0f33.js"
+  }
+]);
